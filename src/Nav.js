7d977806081b544300e9d2b06cd3646f6efcfea2
John <john.dev.work.01@gmail.com>
2020-04-17 00:19:53 -0700

    Add Nav shrink and sticky effect on scroll

--- src/Nav.js
+++ src/Nav.js
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 const Nav = () => {
+  const navEl = useRef(null);
+
+  useEffect(() => {
+    const headerEl = document.getElementById("main-head");
+
+    const onScrollEffects = () => {
+      const distanceY =
+        window.pageYOffset || document.documentElement.scrollTop;
+      const stickOn = headerEl.offsetHeight;
+      const shrinkOn = headerEl.offsetHeight;
+
+      distanceY > stickOn
+        ? navEl.current.classList.add("sticky")
+        : navEl.current.classList.remove("sticky");
+
+      distanceY > shrinkOn
+        ? navEl.current.classList.add("shrunken")
+        : navEl.current.classList.remove("shrunken");
+    };
+
+    window.addEventListener("scroll", onScrollEffects);
+
+    return () => window.removeEventListener("scroll");
+  }, []);
+
   return (
-    <div id="main-nav" className="main-nav">
+    <div ref={navEl} id="main-nav" className="main-nav">
       nav
     </div>
   );
